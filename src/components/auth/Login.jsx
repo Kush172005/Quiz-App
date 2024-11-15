2376fd88c2f88c43da1bacb4133048c389374e54
Kush Agarwal <agrawalkush783@gmail.com>
2024-11-15 16:56:47 +0530

    changed the title of website

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -9,6 +9,7 @@ const Login = () => {
     const [password, setPassword] = useState("");
     const { setIsAuthenticated, isAuthenticated, setUser } = useAuth();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false); 
 
     useEffect(() => {
         if (isAuthenticated) {
@@ -29,6 +30,8 @@ const Login = () => {
             return;
         }
 
+        setLoading(true); 
+
         try {
             const response = await userLogin({ email, password });
             const data = await response.json();
@@ -36,7 +39,6 @@ const Login = () => {
                 toast.success(data.message);
                 localStorage.setItem("authToken", data.token);
                 setUser(data);
-                console.log(data);
                 setIsAuthenticated(true);
                 navigate("/main");
             } else {
@@ -47,62 +49,74 @@ const Login = () => {
         } catch (error) {
             console.error("Login error:", error);
             toast.error("Failed to login. Please try again.");
+        } finally {
+            setLoading(false); // Set loading state to false after login attempt
         }
     };
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-            <div className="flex flex-col gap-4 w-full items-center">
-                <div className="bg-gradient-to-r from-gray-800 to-gray-900 shadow-2xl rounded-xl border-2 border-soft-teal p-8 w-full max-w-md space-y-6">
-                    <h2 className="text-3xl font-bold text-soft-teal text-center">
-                        Login
-                    </h2>
-                    <form onSubmit={handleLogin} className="space-y-4">
-                        <div>
-                            <label className="block text-soft-teal mb-2">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder="Enter your email"
-                                className="w-full p-3 bg-gray-800 border border-soft-teal text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-soft-teal"
-                                autoComplete="email"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-soft-teal mb-2">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Enter your password"
-                                className="w-full p-3 bg-gray-800 border border-soft-teal text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-soft-teal"
-                                autoComplete="password"
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="w-full bg-soft-teal text-black px-6 py-3 rounded-lg hover:bg-muted-purple hover:text-white transition duration-300"
-                        >
-                            Login
-                        </button>
-                        <div className="flex justify-evenly">
-                            <p>Not Registered yet?</p>
-                            <span
-                                onClick={() => navigate("/signup")}
-                                className="text-soft-teal cursor-pointer"
-                            >
-                                Signup
-                            </span>
-                        </div>
-                    </form>
+        <>
+            {loading ? (
+                // Show loading spinner while data is being fetched
+                <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+                    <div className="loader"></div>
+                    <p>Logging in...</p>
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+                    <div className="flex flex-col gap-4 w-full items-center">
+                        <div className="bg-gradient-to-r from-gray-800 to-gray-900 shadow-2xl rounded-xl border-2 border-soft-teal p-8 w-full max-w-md space-y-6">
+                            <h2 className="text-3xl font-bold text-soft-teal text-center">
+                                Login
+                            </h2>
+                            <form onSubmit={handleLogin} className="space-y-4">
+                                <div>
+                                    <label className="block text-soft-teal mb-2">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        placeholder="Enter your email"
+                                        className="w-full p-3 bg-gray-800 border border-soft-teal text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-soft-teal"
+                                        autoComplete="email"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="block text-soft-teal mb-2">
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        placeholder="Enter your password"
+                                        className="w-full p-3 bg-gray-800 border border-soft-teal text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-soft-teal"
+                                        autoComplete="password"
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="w-full bg-soft-teal text-black px-6 py-3 rounded-lg hover:bg-muted-purple hover:text-white transition duration-300"
+                                >
+                                    Login
+                                </button>
+                                <div className="flex justify-evenly">
+                                    <p>Not Registered yet?</p>
+                                    <span
+                                        onClick={() => navigate("/signup")}
+                                        className="text-soft-teal cursor-pointer"
+                                    >
+                                        Signup
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
