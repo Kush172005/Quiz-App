f647ed88478c2895abc18aba9b7b74f05a0f0093
Kush Agarwal <agrawalkush783@gmail.com>
2024-11-27 23:27:20 +0530

    fixed the loader issue on getUser

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,26 +1,35 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { getUser } from "../api/auth";
+import MyLoader from "../components/Loader/MyLoader";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(null);
     const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const token = localStorage.getItem("authToken");
         if (!token) {
             setIsAuthenticated(false);
+            setLoading(false);
             return;
         }
+
         getUser({ accessToken: token })
             .then((response) => response.json())
             .then(({ user }) => {
                 setUser(user);
                 setIsAuthenticated(true);
             })
-            .catch(() => setIsAuthenticated(false));
+            .catch(() => setIsAuthenticated(false))
+            .finally(() => setLoading(false));
     }, []);
+
+    if (loading) {
+        return <MyLoader />;
+    }
 
     return (
         <AuthContext.Provider
